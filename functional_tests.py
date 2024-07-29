711a149e88299306f56ac4d935bf713d868f15d6
nikharsaxena <nikharrakesh@gmail.com>
2024-07-29 00:18:12 -0700

    Functiona test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,9 @@
+import time
 from typing import Generator
 from selenium import webdriver
 from selenium.webdriver import Safari
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import pytest
 
 
@@ -15,3 +18,18 @@ def test_can_start_a_list_and_retrieve_it_later(browser):
     browser.get('http://localhost:8000')
 
     assert "To-Do" in browser.title
+    header_text = browser.find_element(By.TAG_NAME, 'h1').text
+    assert 'To-Do' in header_text
+    
+    input_box = browser.find_element(By.ID, 'id_new_item')
+    assert input_box.get_attribute('placeholder') == 'Enter a to-do item'
+    
+    input_box.send_keys('Buy peacock feathers')
+    
+    input_box.send_keys(Keys.ENTER)
+    time.sleep(1)
+    
+    table = browser.find_element(By.ID, 'id_list_table')
+    rows = table.find_elements(By.TAG_NAME, 'tr')
+    assert any(row.text == '1: Buy peacock feathers' for row in rows)
+
